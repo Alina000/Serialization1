0e64c457ed30037bacee0a34f46a412d71f17a3b
Alina000 <ahmetzyanov-1996@mail.ru>
2017-11-14 12:16:10 +0300

    'Initial'

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    public class Output
+    {
+        public decimal SumResult { get; set; }
+        public int MulResult { get; set; }
+        public decimal[] SortedInputs { get; set; }
+    }
+
+    public class Input
+    {
+        public int K { get; set; }
+        public decimal[] Sums { get; set; }
+        public int[] Muls { get; set; }
+    }
+
+    class Program
+    {
+        static public Output Result(Input b)
+        {
+            Output a = new Output();
+            a.MulResult = b.Muls.Aggregate((p, x) => p = p * x);
+            a.SumResult = b.Sums.Sum() * b.K;
+            List<decimal> q = b.Sums.ToList();
+            int i = 0;
+            while (i < b.Muls.Length)
+            {
+                q.Add(Convert.ToDecimal(b.Muls[i]));
+                i++;
+            }
+            q.Sort();
+            a.SortedInputs = q.ToArray();
+            return a;
+        }
+
+        static void Main(string[] args)
+        {
+            String str1 = Console.ReadLine();
+            String str2 = Console.ReadLine();
+            if (str1 == "Json")
+            {
+                String str3 = JsonConvert.SerializeObject(Result(JsonConvert.DeserializeObject<Input>(str2)));
+                Console.WriteLine(str3.Replace(Environment.NewLine, "").Replace(" ", ""));
+            }
+            if (str1 == "Xml")
+            {
+                XmlSerializer res = new XmlSerializer(typeof(Input));
+                Output x = Result((Input)res.Deserialize(new StringReader(str2)));
+                MemoryStream temp = new MemoryStream();
+                res = new XmlSerializer(typeof(Output));
+                res.Serialize(temp, x);
+                String str3 = Encoding.UTF8.GetString(temp.ToArray());
+                str3 = str3.Remove(0, str3.IndexOf('>') + 1);
+                str3 = str3.Remove(str3.IndexOf(' '), str3.IndexOf('>', str3.IndexOf(' ')) - str3.IndexOf(' ')).Replace(Environment.NewLine, string.Empty).Replace(" ", string.Empty);
+                Console.WriteLine(str3);
+            }
+        }
+    }
+}
